29bbad356c9afd5803cf0247ddd4a9d135f8e263
AvniChandak <chandakavni2006@gmail.com>
2026-04-10 14:25:16 +0530

    Save my current work before sync

---
+++ frontend/src/pages/MindMap.jsx
@@ -0,0 +1,15 @@
+export default function MindMap() {
+  return (
+    <iframe
+      src="/mindmap.html"
+      title="Dance Forms Mind Map"
+      style={{
+        width: "100%",
+        height: "calc(100vh - 200px)",
+        border: "none",
+        borderRadius: "16px",
+        display: "block",
+      }}
+    />
+  );
+}
